e2425dd7deb3a4fe5209636fe98433e329b19a33
ipal <72238450+petelpop@users.noreply.github.com>
2024-02-21 09:18:29 +0700

    create banner

--- lib/view/homescreen.dart
+++ lib/view/homescreen.dart
@@ -10,9 +10,94 @@ class Homescreen extends StatelessWidget {
     return Scaffold(
       appBar: _appBar(),
       bottomNavigationBar: _bottomNavigationBar(),
-      
+      body: DefaultTabController(length: 4, 
+          child: Padding(padding: EdgeInsets.symmetric(horizontal: 24),
+              child: NestedScrollView(
+              headerSliverBuilder: (BuildContext context, bool innerBoxisScrolled) => [
+                SliverToBoxAdapter(
+                  child: _salam(),
+                ),
+                SliverAppBar(
+                  automaticallyImplyLeading: false,
+                )
+              ], 
+              body: TabBarView(
+                children: []
+                )
+                ),
+                )  
+                ),
     );
   }
+
+  Column _salam() => Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('Assalamu\'alaikum',
+        style: GoogleFonts.plusJakartaSans(
+          color: Colors.black,
+          fontWeight: FontWeight.w500,
+          fontSize: 24
+        ),),
+        SizedBox(height: 5,),
+        Text('ipal',
+        style: GoogleFonts.plusJakartaSans(
+          color: Colors.black,
+          fontWeight: FontWeight.bold,
+          fontSize: 30
+        ),),
+        SizedBox(height: 15),
+        _lastRead()
+      ],
+  ) ;
+
+  Stack _lastRead() => Stack(
+    children: [
+      Container(
+        height: 160,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          gradient: const LinearGradient(
+            colors: [Color(0xFFDF98FA), Color(0xFF9055FF)],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight
+          )
+        ),
+      ),
+      Positioned(
+        bottom: 0, 
+        right: 0, 
+        child: SvgPicture.asset('assets/svg/quran_banner.svg'),
+      ),
+      Padding(padding: EdgeInsets.all(20),
+      child:  Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 8),
+          Text('Last Read',
+          style: GoogleFonts.poppins(
+            color: Colors.white,
+            fontSize: 16,
+          ),
+          ),
+          SizedBox(height: 16),
+          Text('Al Fatihah',
+          style: GoogleFonts.poppins(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold
+          ),
+          ),
+          SizedBox(height: 4),
+          Text('Ayat Ke-1',
+          style: GoogleFonts.poppins(
+            color: Colors.white,
+            fontSize: 16
+          ))
+        ],
+      ),)
+    ],
+  );
 
   AppBar _appBar() => AppBar(
           title: Row(
