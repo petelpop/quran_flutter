c04024830c2d6f83ed8a7c95c7359f249c5e4b6f
ipal <72238450+petelpop@users.noreply.github.com>
2024-02-21 08:34:50 +0700

    create appbar and bottom navbar

--- lib/view/homescreen.dart
+++ lib/view/homescreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Homescreen extends StatelessWidget {
   const Homescreen({super.key});
@@ -6,7 +8,52 @@ class Homescreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: _appBar(),
+      bottomNavigationBar: _bottomNavigationBar(),
       
     );
   }
+
+  AppBar _appBar() => AppBar(
+          title: Row(
+            children: [
+              SvgPicture.asset('assets/svg/quran_icon.svg'),
+              SizedBox(
+                width: 16,
+              ),
+              Text(
+                'Quran App',
+                style: GoogleFonts.poppins(
+                    color: Colors.purple,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+          automaticallyImplyLeading: false,
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: SvgPicture.asset('assets/svg/search_icon.svg'))
+          ]);
+
+  BottomNavigationBar _bottomNavigationBar() => BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.white,
+        items: [
+          _bottomNavigationBarItem(
+              icon: 'assets/svg/quran_icon.svg', label: 'Quran'),
+          _bottomNavigationBarItem(
+              icon: 'assets/svg/doa_icon.svg', label: 'Doa'),
+          _bottomNavigationBarItem(
+              icon: 'assets/svg/bookmark_icon.svg', label: 'Bookmark'),
+        ],
+      );
+
+  BottomNavigationBarItem _bottomNavigationBarItem(
+          {required String icon, required String label}) =>
+      BottomNavigationBarItem(
+          icon: SvgPicture.asset(icon),
+          activeIcon: SvgPicture.asset(icon),
+          label: label);
 }
