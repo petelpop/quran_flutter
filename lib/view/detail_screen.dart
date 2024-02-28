46ba28a1ca67fd861d190bf3b75bca057ce67a50
ipal <72238450+petelpop@users.noreply.github.com>
2024-02-28 08:25:42 +0700

    create banner at detail_screen.dart

--- lib/view/detail_screen.dart
+++ lib/view/detail_screen.dart
@@ -19,59 +19,140 @@ class DetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: _detailSurah(), 
-      builder: ((context,snapshot) {
-        // init data from response to model
-        if(!snapshot.hasData){
+        future: _detailSurah(),
+        builder: ((context, snapshot) {
+          // init data from response to model
+          if (!snapshot.hasData) {
             return const Center(
               child: CircularProgressIndicator(),
             );
-        }
-        Surah surah = snapshot.data!;
-        return Scaffold(
-          backgroundColor: Colors.white,
-          appBar: _appBar(context: context, surah: surah),
-          body: NestedScrollView(headerSliverBuilder: ((context, innerBoxIsScrolled) => [
-            SliverToBoxAdapter(
-              child: _detail(context: context, surah: surah),
-            )
-          ]), body: Container()),
-        );
-      }));
+          }
+          Surah surah = snapshot.data!;
+          return Scaffold(
+            backgroundColor: Colors.white,
+            appBar: _appBar(context: context, surah: surah),
+            body: NestedScrollView(
+                headerSliverBuilder: ((context, innerBoxIsScrolled) => [
+                      SliverToBoxAdapter(
+                        child: _detailBanner(context: context, surah: surah),
+                      )
+                    ]),
+                body: Container()),
+          );
+        }));
   }
 
-  Padding _detail({required BuildContext context, required Surah surah}) => Padding(
-    padding: const EdgeInsets.symmetric(horizontal: 24),
-    child: Stack(
-      children: [
-        Text(surah.namaLatin.toString())
-      ],
-    ),
-  );
-
-  AppBar _appBar({required BuildContext context, required Surah surah}) => AppBar(
-    backgroundColor: Colors.white,
-    elevation: 0,
-    // hide the back button
-    automaticallyImplyLeading: false,
-    title: Row(
-      children: [
-        IconButton(
-          // to make a button back to the prev page when pressed
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: SvgPicture.asset('assets/svg/back_icon.svg')),
-          const SizedBox(width: 24),
-          Text(
-            surah.namaLatin.toString(),
-            style: GoogleFonts.poppins(
-              color: const Color(0xFF672CBC),
-              fontSize: 20,
-              fontWeight: FontWeight.bold
+  Padding _detailBanner(
+          {required BuildContext context, required Surah surah}) =>
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Stack(
+          children: [
+            Container(
+              height: 265,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  gradient: const LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: [
+                        Color(0xFFDF98FA),
+                        Color(0xFF9055FF)
+                        ]
+                      )
+                    ),
             ),
-          )
-      ],
-    ),
-  );
+            Positioned(
+              bottom: 0,
+              right: 0,
+              child: Opacity(
+                opacity: 0.2,
+                child: SvgPicture.asset(
+                  'assets/svg/quran_banner.svg',
+                  width: 324 - 58,
+                ),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  Text(
+                    surah.namaLatin,
+                    style: GoogleFonts.poppins(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 26
+                    ),
+                  ),
+                  const SizedBox(height: 4),
+                  Text(
+                    surah.arti,
+                    style: GoogleFonts.poppins(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 16
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Divider(
+                    color: Colors.white.withOpacity(0.35), 
+                    thickness: 2,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        surah.tempatTurun.toString(),
+                        style: GoogleFonts.poppins(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 14
+                        ),
+                      ),
+                      const SizedBox(width: 5),
+                      Text(
+                        '${surah.jumlahAyah} Ayat',
+                        style: GoogleFonts.poppins(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 38),
+                  SvgPicture.asset('assets/svg/basmallah.svg')
+                ],
+              ),
+            )
+          ],
+        ),
+      );
+
+  AppBar _appBar({required BuildContext context, required Surah surah}) =>
+      AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        // hide the back button
+        automaticallyImplyLeading: false,
+        title: Row(
+          children: [
+            IconButton(
+                // to make a button back to the prev page when pressed
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: SvgPicture.asset('assets/svg/back_icon.svg')),
+            const SizedBox(width: 24),
+            Text(
+              surah.namaLatin.toString(), 
+              style: GoogleFonts.poppins(
+                  color: const Color(0xFF672CBC),
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+      );
 }
